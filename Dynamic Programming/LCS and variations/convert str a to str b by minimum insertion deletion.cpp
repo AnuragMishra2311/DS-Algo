a7507a168f90d6e61fe1a56f91242cc32c858ab5
Anurag Mishra <anuragmishra2311@gmail.com>
2021-05-29 14:25:02 +0530

    convert str a to str b

--- Dynamic Programming/LCS and variations/convert str a to str b by minimum insertion deletion.cpp
+++ Dynamic Programming/LCS and variations/convert str a to str b by minimum insertion deletion.cpp
@@ -1,11 +1,14 @@
+/*
+
 https://practice.geeksforgeeks.org/problems/minimum-number-of-deletions-and-insertions0209/1#
 
 Given two strings str1 and str2. The task is to remove or insert the minimum number of characters from/in str1 so as to transform it into str2. 
 It could be possible that the same character needs to be removed/deleted from one point of str1 and inserted to some another point.
 
 
+*/
 
-*******************************************************************************************************************************************************************
+
 
 int minOperations(string str1, string str2) 
 { 
@@ -32,6 +35,3 @@ int minOperations(string str1, string str2)
 	int lcs=dp[n][m];
 	return n+m-(2*lcs);
 }
-
-
-*******************************************************************************************************************************************************************
