6099a10a1a1cf3ee61dec530292943554008efb8
Anurag Mishra <anuragmishra2311@gmail.com>
2021-05-30 18:37:03 +0530

    Hello Fibonacci

--- Dynamic Programming/Fibonacci and variations/Fibonacci.cpp
+++ Dynamic Programming/Fibonacci and variations/Fibonacci.cpp
@@ -1,9 +1,10 @@
+//Top Down Approach
+
+
 vector<long long>memo(1000,-1);
 
 long long findNthFibonacci(int number)
 {
-        long long res;
-        
         if(memo[number]==-1)
         {
             if(number==0 || number==1)
@@ -17,4 +18,22 @@ long long findNthFibonacci(int number)
         }
         return memo[number];
         
+}
+
+
+// Bottom Up Approach
+
+
+long long findNthFibonacci(int number)
+{
+        // Your Code Here
+        long long int dp[number+1];
+        dp[0]=0;
+        dp[1]=1;
+
+        for(int i=2;i<=number;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[number];
 }
