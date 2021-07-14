646328d0b8f006afdb32027cc11982d2c5be4497
Anurag Mishra <anuragmishra2311@gmail.com>
2021-07-14 18:25:58 +0530

    optimised approach

--- Trees/Balanced tree.cpp
+++ Trees/Balanced tree.cpp
@@ -1,8 +1,43 @@
+// https://practice.geeksforgeeks.org/problems/check-for-balanced-tree/1#
+
 /*
 
-https://practice.geeksforgeeks.org/problems/check-for-balanced-tree/1#
+Approach:  Keep appending abs(left_height - right_height) in a vector. Traverse the vector if any element is greater
+than 1, return false else return true.
+
+Time complexity : O(N)
+Space Complexity : O(N)
 
 */
+
+int height(Node *root, vector<int> &store)
+{
+    if(!root)
+        return 0;
+        
+    int left = height(root->left, store);
+    int right = height(root->right, store);
+    
+    int ans = 1 + max(left, right);
+    store.push_back(abs(left-right));
+    return ans;
+    
+}
+
+bool isBalanced(Node *root)
+{
+    vector<int>store;
+    height(root, store);
+    
+    for(auto i:store)
+    {
+        if(i>1)
+            return false;
+    }
+    
+    return true;
+}
+
 
 // O(N^2) Time Complexity
 
