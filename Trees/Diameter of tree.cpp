4798aeb62a26399c2467d2ae93821170f604d351
Anurag Mishra <55075448+AnuragMishra2311@users.noreply.github.com>
2021-10-10 20:37:52 +0530

    Added optimized approach

--- Trees/Diameter of tree.cpp
+++ Trees/Diameter of tree.cpp
@@ -21,3 +21,27 @@ int height(Node *root)
         
         return max(left + right + 1, max(diameter(root->left), diameter(root->right)));
     }
+
+
+// Time Complexity : O(N)
+// Space Complexity : O(H)
+
+int solution(Node *root, int &ans)
+    {
+        if(root == NULL)
+            return 0;
+            
+        int left_height = solution(root->left, ans);
+        int right_height = solution(root->right, ans);
+        
+        ans = max(ans, left_height + right_height + 1);
+        
+        return 1 + max(left_height, right_height);
+    }
+    
+    int diameter(Node* root) 
+    {
+        int ans = INT_MIN;
+        solution(root, ans);
+        return ans;
+    }
